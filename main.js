b2f82d885e14ea4bea6cc6f642480d0d659f99d0
damir <damir.svrtan@gmail.com>
2013-10-23 00:18:15 +0200

    Added the .js file w/jQuery

---
+++ main.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $('#speisekarte #circluar-pics .span2').on('click',function(){
+    $(this).css('background-position-y', '-629px')
+    $(this).css('background-position-x', '-194px')
+  })
+});
