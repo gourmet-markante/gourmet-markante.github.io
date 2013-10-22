b93eb7433a378f92a5c080fe2a89f0b2120a8bfe
damir <damir.svrtan@gmail.com>
2013-10-23 01:05:51 +0200

    Added on hover speisekarte highlight

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
 $(document).ready(function(){
-  $('#speisekarte #circluar-pics .span2').on('click',function(){
+
+  $('#speisekarte #circluar-pics .span2').on('mouseover',function(){
+    $('#speisekarte #circluar-pics .span2').css('background-position-x', '');
+    $('#speisekarte #circluar-pics .span2').css('background-position-y', '');
+
     $(this).css('background-position-y', '-629px')
-    $(this).css('background-position-x', '-194px')
-  })
+    $(this).css('background-position-x', '-=3')
+
+    var order_number = $(this).index('#speisekarte #circluar-pics .span2') + 1
+    $('#speisekarte #speisekarte-titles .span2').css('visibility', 'hidden');
+    $('#speisekarte #speisekarte-titles .span2:nth-of-type('+order_number+')').css('visibility', 'visible');
+  });
+
+  $('#speisekarte #circluar-pics').on('mouseleave', function(){
+    $('#speisekarte #circluar-pics .span2').css('background-position-x', '');
+    $('#speisekarte #circluar-pics .span2').css('background-position-y', '');
+    $('#speisekarte #speisekarte-titles .span2').css('visibility', 'hidden');
+  });
+
+
 });
